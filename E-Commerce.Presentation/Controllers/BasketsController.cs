28a86a602b54f20c8928c68d0d0a011bd8e164f6
Rojena Shehata <rojenashehata@gmail.com>
2025-11-25 19:02:32 +0200

    Base of Basket Module - use Redis

---
+++ E-Commerce.Presentation/Controllers/BasketsController.cs
@@ -0,0 +1,46 @@
+﻿using E_Commerce.ServicesAbstraction;
+using E_Commerce.Shared.DTOs.BasketDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BasketsController: ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketsController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<BasketDTO> >GetBasket(string id)
+        {
+            var basket=await _basketService.GetBasketAsync(id);
+            return Ok(basket);
+        }
+        [HttpPost]
+        public async Task<ActionResult<BasketDTO>> CreateOrUpdateBasket(BasketDTO basketInput)
+        {
+            var basket = await _basketService.CreateOrUpdateBasketAsync(basketInput);
+            return Ok(basket);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteBasket(string id)
+        {
+            var basket = await _basketService.DeleteBasketAsync(id);
+            return Ok(basket);
+
+        }
+
+
+    }
+}
---
+++ E-Commerce.Services/BasketService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities.BasketModule;
+using E_Commerce.ServicesAbstraction;
+using E_Commerce.Shared.DTOs.BasketDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
+
+        public BasketService(IBasketRepository basketRepository,IMapper mapper)
+        {
+            _basketRepository = basketRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<BasketDTO> CreateOrUpdateBasketAsync(BasketDTO basket)
+        {
+            var customerBasket = _mapper.Map<BasketDTO, CustomerBasket>(basket);
+          var createdOrUpdatedBasket= await _basketRepository.CreateOrUpdateBasketAsync(customerBasket);
+            
+            return _mapper.Map<CustomerBasket, BasketDTO>(createdOrUpdatedBasket);
+
+        }
+
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _basketRepository.DeleteBasketAsync(basketId);
+        }
+
+        public async Task<BasketDTO> GetBasketAsync(string basketId)
+        {
+           var customerBasket= await _basketRepository.GetBasketAsync(basketId);
+
+            return  _mapper.Map<CustomerBasket,BasketDTO>(customerBasket);
+        }
+    }
+}
