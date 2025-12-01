417f4ff8656934ae31771f5b7c52bc6f79dc9b20
Rojena Shehata <rojenashehata@gmail.com>
2025-12-02 01:06:52 +0200

    Implement Result Pattern

---
+++ E-Commerce.Shared/CommonResult/Error.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.CommonResult
+{
+    public class Error
+    {
+
+        public string Code { get; }
+        public string Description { get;  }
+        public ErrorType type { get;  }
+
+        private Error(string code, string description, ErrorType type)
+        {
+            Code = code;
+            Description = description;
+            this.type = type;
+        }
+
+        //Static Factory Methods  To Create Errors
+        public static Error Failure(string code = "General.Failure", string description = "General Failure Has Occurred")
+        {
+            return new Error(code, description, ErrorType.Failure);
+        }
+        
+        public static Error NotFound(string code = "General.NotFound", string description = "The Request Resource Was Not Found")
+        {
+            return new Error(code, description, ErrorType.NotFound);
+        }
+        public static Error UnAuthorized(string code = "General.UnAuthorized", string description = "You Are Not Authorized")
+        {
+            return new Error(code, description, ErrorType.UnAuthorized);
+        }
+        public static Error Validation(string code = "General.Validation", string description = "Validation Error Has Occurred")
+        {
+            return new Error(code, description, ErrorType.Validation);
+        }
+        public static Error Forbidden(string code = "General.Forbidden", string description = "You do not have permission to access this resource")
+        {
+            return new Error(code, description, ErrorType.Forbidden);
+        }
+        public static Error InvalidCredentials(string code = "General.InvalidCredentials", string description = "Authentication failed due to invalid credentials.")
+        {
+            return new Error(code, description, ErrorType.InvalidCredentials);
+        }
+    }
+}
---
+++ E-Commerce.Shared/CommonResult/ErrorType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.CommonResult
+{
+    public enum ErrorType
+    {
+        Failure = 0,
+        NotFound = 1,
+        UnAuthorized = 2,
+        Validation,
+        Forbidden,
+        InvalidCredentials
+    }
+}
