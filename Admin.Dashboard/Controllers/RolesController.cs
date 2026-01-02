f8cf287bed6da366ddb5b09a39b05bbb1cb0e160
Rojena Shehata <rojenashehata@gmail.com>
2026-01-02 23:14:52 +0200

    feat(Role): Add Get All Roles Action And Index View style(_AdminLayout): update Bootstrap Layout Styling

---
+++ Admin.Dashboard/Controllers/RolesController.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerce.Services;
+using E_Commerce.ServicesAbstraction;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Admin.Dashboard.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly IRoleService _roleService;
+
+        public RolesController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles=await _roleService.GetAllRolesAsync();
+            return View(roles);
+        }
+    }
+}
---
+++ E-Commerce.Shared/AdminDashboardViewModels/RoleViewModels/RoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.AdminDashboardViewModels
+{
+    public record RoleViewModel
+    {
+        public string Id { get; init; } = default!;
+        public string Name { get; init; }=default!;
+    }
+}
